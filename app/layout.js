2d7cc883b95e00b32db248c456b0ff1cc308f032
JanKostalek <jan.kostalek@gmail.com>
2026-03-03 15:29:21 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,17 @@
 import "./globals.css";
+import { DM_Sans, Space_Grotesk } from "next/font/google";
+
+const bodyFont = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-body"
+});
+
+const headingFont = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  variable: "--font-heading"
+});
 
 export const metadata = {
   title: "Hlidacka bazaru",
@@ -8,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="cs">
-      <body>{children}</body>
+      <body className={`${bodyFont.variable} ${headingFont.variable}`}>{children}</body>
     </html>
   );
 }
